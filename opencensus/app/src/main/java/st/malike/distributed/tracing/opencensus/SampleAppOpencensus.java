62982aa0822327437bd4b2a44cba8fe5d110b542
malike <st.malike@gmail.com>
2018-03-09 23:11:08 +0000

    sample app for opencensus distributed tracing with elasticsearch trace exporter

---
+++ opencensus/app/src/main/java/st/malike/distributed/tracing/opencensus/SampleAppOpencensus.java
@@ -0,0 +1,16 @@
+package st.malike.distributed.tracing.opencensus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * malike_st
+ */
+public class SampleAppOpencensus {
+
+    public static void main(String[] args) throws Exception {
+    }
+
+
+}
