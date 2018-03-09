c26db426b482814d8e295a971b8d9925b636392d
malike <st.malike@gmail.com>
2018-03-09 23:15:51 +0000

    sample app for distributed tracing using spring sleuth

---
+++ spring-cloud-sleuth/app/src/main/java/st/malike/distributed/tracing/sleuth/app/SleuthSampleApp.java
@@ -0,0 +1,19 @@
+package st.malike.distributed.tracing.sleuth.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * malike_st
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SleuthSampleApp {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SleuthSampleApp.class, args);
+    }
+
+
+}
