9c2528b02617f4d7a4b275cb47bdb1a4df74bac9
malike <st.malike@gmail.com>
2018-03-11 23:34:15 +0000

    set up for opencensus project, which would export trace data to zipkin and elasticsearch using plugin I built

---
+++ opencensus/src/main/java/st/malike/distributed/tracing/opencensus/SampleAppOpencensus.java
@@ -0,0 +1,12 @@
+package st.malike.distributed.tracing.opencensus;
+
+/**
+ * malike_st
+ */
+public class SampleAppOpencensus {
+
+  public static void main(String[] args) throws Exception {
+  }
+
+
+}
