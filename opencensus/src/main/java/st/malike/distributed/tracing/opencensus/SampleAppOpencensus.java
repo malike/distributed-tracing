cc347bc16ff06201b1dcfb724d6de0ad45213063
malike <st.malike@gmail.com>
2018-03-17 17:21:41 +0000

    opencensus integratio with elasticsearch trace exporter

--- opencensus/src/main/java/st/malike/distributed/tracing/opencensus/SampleAppOpencensus.java
+++ opencensus/src/main/java/st/malike/distributed/tracing/opencensus/SampleAppOpencensus.java
@@ -1,12 +1,24 @@
 package st.malike.distributed.tracing.opencensus;
 
+import io.opencensus.exporter.trace.elasticsearch.ElasticsearchConfiguration;
+import io.opencensus.exporter.trace.elasticsearch.ElasticsearchTraceExporter;
+
 /**
  * malike_st
  */
 public class SampleAppOpencensus {
 
+  private final static String ELASTIC_SEARCH_URL= "http://localhost:9200";
+  private final static String INDEX_FOR_TRACE= "opencensus";
+  private final static String TYPE_FOR_TRACE= "trace";
+
+
   public static void main(String[] args) throws Exception {
+
+    ElasticsearchConfiguration elasticsearchConfiguration
+        = new ElasticsearchConfiguration(null, null,ELASTIC_SEARCH_URL,
+        INDEX_FOR_TRACE,TYPE_FOR_TRACE);
+    ElasticsearchTraceExporter.createAndRegister(elasticsearchConfiguration);
+
   }
-
-
 }
