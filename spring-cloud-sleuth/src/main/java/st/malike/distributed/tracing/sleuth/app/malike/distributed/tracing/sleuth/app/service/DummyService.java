f4879300d9b8a0b9c39471b7a4de04f1510b2897
malike <st.malike@gmail.com>
2018-04-01 22:46:31 +0000

    upgraded zipkin servre to get the v2 api

--- spring-cloud-sleuth/src/main/java/st/malike/distributed/tracing/sleuth/app/malike/distributed/tracing/sleuth/app/service/DummyService.java
+++ spring-cloud-sleuth/src/main/java/st/malike/distributed/tracing/sleuth/app/malike/distributed/tracing/sleuth/app/service/DummyService.java
@@ -2,6 +2,7 @@ package st.malike.distributed.tracing.sleuth.app.malike.distributed.tracing.sleu
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -20,11 +21,11 @@ public class DummyService {
   @Value("${server.port:8080}")
   private int port;
 
+  @NewSpan("getDummy()")
   public String getDummy() {
-
     HttpHeaders headers = new HttpHeaders();
     headers.add("Content-Type", "application/json");
-    HttpEntity<?> entity = new HttpEntity<>(headers);
+    HttpEntity<?> entity = new HttpEntity<Object>(headers);
     ResponseEntity<String> response = restTemplate.exchange("http://localhost:" +
         port + "/api/another/dummy/" + 3, HttpMethod.GET, entity, String.class);
     return response.getBody();
